c86c750c1a3c6f81c4cdb9993019deea81cf359b
Brian Heise <92563897+bnheise@users.noreply.github.com>
2023-06-05 10:25:17 +0900

    added explanatory comments

--- customWidget.js
+++ customWidget.js
@@ -1,3 +1,7 @@
+// You may completely replace this file with any content that you want.
+// Just keep the file in this location and do not change its name so that
+// it will continue to render properly in the HTML file.
+
 class EmployeeRecognition extends HTMLElement {
   constructor() {
     super();
